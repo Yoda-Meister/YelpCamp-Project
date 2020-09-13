3fc9eeb797c3a0f00be785aca20b91523875e3cf
Waleed Joda <Waleedjoda@gmail.com>
2020-09-13 18:35:19 +0200

    More ejs

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) =>{
-    res.send("home page")
+    res.render("landing")
+})
+
+app.get("/asthetic", (req, res) =>{
+    let campgrounds = [
+        {name: "Salmon Creek", image: ""},
+        {name: "Garanite Hill", image: ""},
+        {name: "Ganat camp", image: ""}
+    ]
+    res.render("asthetic")
 })
 
 app.listen(8000, process.env.IP, () =>{
