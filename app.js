3a5a9eada1c38cb37968b3c5ba82d68fa9c6786c
Yoda-Meister <waleedjoda@gmail.com>
2020-10-18 21:01:29 +0200

    Implemented mongodb

--- app.js
+++ app.js
@@ -1,30 +1,62 @@
-const express = require("express");
-const { response } = require("express");
-const app = express();
+const express = require("express"),
+     rp       = require("request-promise"),
+     app      = express(),
+     bodyParser = require("body-parser"),
+     mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+const aestheticSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Aesthetics = mongoose.model("Aesthetics", aestheticSchema);
+
+// Aesthetics.create(
+//     {
+//         name: "Garanite Hill",
+//         image: "https://i.ytimg.com/vi/-ROAj71OiUU/maxresdefault.jpg"
+//     },(err,aesthetic) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(aesthetic);
+//         }
+//     });
 
 app.get("/", (req, res) =>{
     res.render("landing")
 })
 
 app.get("/aesthetic", (req, res) =>{
-    let aesthetic = [
-        {
-            name: "Salmon Creek",
-            image: "https://i.pinimg.com/originals/1c/74/30/1c743094b32e4fdf2c448bcba8bd4161.jpg"
-        },
-        {
-            name: "Garanite Hill",
-            image: "https://i.ytimg.com/vi/-ROAj71OiUU/maxresdefault.jpg"
-        },
-        {
-            name: "Ganat camp",
-            image: "https://wallpapercave.com/wp/wp4751466.jpg"
+    Aesthetics.find({}, (err,aesthetic) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("aesthetic", {aesthetic:aesthetic});
         }
-    ]
-    res.render("aesthetic", {aesthetic:aesthetic});
-})
+    })
+});
+
+app.get("/aesthetic/new", (req, res) =>{
+    res.render("new.ejs")
+});
+
+app.post("/aesthetic", (req, res) => {
+    const name = req.body.name;
+    const image = req.body.image;
+    const newArt = {name: name, image: image}
+    Aesthetics.create(newArt, (err, newlyCreated) =>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/aesthetic");
+        }
+    } )
+});
 
 app.listen(8000, process.env.IP, () =>{
     console.log("servers running")
